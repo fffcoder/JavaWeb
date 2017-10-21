3aca222d41d1002ba810a83e5cf2e9295821ec1b
fengluyan <466652855@qq.com>
2017-10-22 03:10:01 +0800

    configHelper

---
+++ src/main/java/com/fengluyan/JavaWeb/constant/ConfigConstant.java
@@ -0,0 +1,17 @@
+package com.fengluyan.JavaWeb.constant;
+
+/**
+ * Created by fly on 10/22/17.
+ */
+public interface ConfigConstant {
+    String CONFIG_FILE = "fly.properties";
+
+    String JDBC_DRIVER = "fly.jdbc.driver";
+    String JDBC_URL = "fly.jdbc.url";
+    String JDBC_USERNAME = "fly.jdbc.username";
+    String JDBC_PASSWORD = "fly.jdbc.password";
+
+    String APP_BASE_PACKAGE = "fly.app.base_package";
+    String APP_JSP_PATH = "fly.app.jsp_path";
+    String APP_ASSET_PATH = "fly.app.asset_path";
+}
