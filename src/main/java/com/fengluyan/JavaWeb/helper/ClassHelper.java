99eb3fe44efd426fa8b16196537666f119ec41f2
fengluyan <466652855@qq.com>
2017-10-22 04:50:40 +0800

    classHelper

---
+++ src/main/java/com/fengluyan/JavaWeb/helper/ClassHelper.java
@@ -0,0 +1,52 @@
+package com.fengluyan.JavaWeb.helper;
+
+import com.fengluyan.JavaWeb.annotation.Controller;
+import com.fengluyan.JavaWeb.annotation.Service;
+import com.fengluyan.JavaWeb.util.ClassUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by fly on 10/22/17.
+ */
+public class ClassHelper {
+    private static final Set<Class<?>> CLASS_SET;
+
+    static {
+        String basePackage = ConfigHelper.getAppBasePackage();
+        CLASS_SET = ClassUtil.getClassSet(basePackage);
+    }
+
+    public static Set<Class<?>> getClassSet() {
+        return CLASS_SET;
+    }
+
+    public static Set<Class<?>> getServiceClassSet() {
+        Set<Class<?>> classSet = new HashSet<>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Service.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    public static Set<Class<?>> getControllerClassSet() {
+        Set<Class<?>> classSet = new HashSet<>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Controller.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    public static Set<Class<?>> getBeanClassSet() {
+        Set<Class<?>> beanClassSet = new HashSet<>();
+        beanClassSet.addAll(getControllerClassSet());
+        beanClassSet.addAll(getServiceClassSet());
+        return beanClassSet;
+    }
+
+}
