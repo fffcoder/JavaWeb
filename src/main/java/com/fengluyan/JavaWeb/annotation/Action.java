05f7db4f5d62147512e87f221e9425ab23c661ce
fengluyan <466652855@qq.com>
2017-10-22 04:33:22 +0800

    annotation

---
+++ src/main/java/com/fengluyan/JavaWeb/annotation/Action.java
@@ -0,0 +1,15 @@
+package com.fengluyan.JavaWeb.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by fly on 10/22/17.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Action {
+    String value();
+}
---
+++ src/main/java/com/fengluyan/JavaWeb/annotation/Inject.java
@@ -0,0 +1,14 @@
+package com.fengluyan.JavaWeb.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by fly on 10/22/17.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inject {
+}
