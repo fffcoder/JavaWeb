4c0fafab13c7d5b5e33ba843c3735c5dc38ccfad
fengluyan <466652855@qq.com>
2017-10-22 05:07:13 +0800

    BeanHelper

---
+++ src/main/java/com/fengluyan/JavaWeb/helper/BeanHelper.java
@@ -0,0 +1,34 @@
+package com.fengluyan.JavaWeb.helper;
+
+import com.fengluyan.JavaWeb.util.ReflectionUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by fly on 10/22/17.
+ */
+public final class BeanHelper {
+    private static final Map<Class<?>, Object> BEAN_MAP = new HashMap<>();
+
+    static {
+        Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();
+        for(Class<?> beanClass : beanClassSet) {
+            Object obj = ReflectionUtil.newInstance(beanClass);
+            BEAN_MAP.put(beanClass, obj);
+        }
+    }
+
+    public static Map<Class<?>, Object> getBeanMap() {
+        return BEAN_MAP;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(Class<T> cls) {
+        if(BEAN_MAP.containsKey(cls)) {
+            throw new RuntimeException("can not get bean by class: " + cls);
+        }
+        return (T) BEAN_MAP.get(cls);
+    }
+}
