de093173933cc3480b963b2ab278cb0c9338610c
fengluyan <466652855@qq.com>
2017-10-22 14:55:26 +0800

    bean/Data

---
+++ src/main/java/com/fengluyan/JavaWeb/bean/Data.java
@@ -0,0 +1,16 @@
+package com.fengluyan.JavaWeb.bean;
+
+/**
+ * Created by fly on 10/22/17.
+ */
+public class Data {
+    private Object model;
+
+    public Data(Object model) {
+        this.model = model;
+    }
+
+    public Object getModel() {
+        return model;
+    }
+}
