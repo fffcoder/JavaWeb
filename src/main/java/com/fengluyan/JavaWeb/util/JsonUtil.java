b2c568c6f05a55a199982ec16a1338156e90f5fc
fengluyan <466652855@qq.com>
2017-11-01 16:20:31 +0800

    jackson -> fastjson

--- src/main/java/com/fengluyan/JavaWeb/util/JsonUtil.java
+++ src/main/java/com/fengluyan/JavaWeb/util/JsonUtil.java
@@ -1,40 +1,24 @@
 package com.fengluyan.JavaWeb.util;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
 
 /**
  * Created by fly on 10/22/17.
  */
 public final class JsonUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonUtil.class);
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     public static <T> String toJson(T obj) {
         String json;
-        try {
-            json = OBJECT_MAPPER.writeValueAsString(obj);
-        } catch (JsonProcessingException e) {
-            LOGGER.error("convert POJO to JSON failure", e);
-            throw new RuntimeException(e);
-        }
+        json = JSON.toJSONString(obj);
         return json;
     }
 
     public static <T> T fromJson(String json, Class<T> type) {
         T pojo;
-        try {
-            pojo = OBJECT_MAPPER.readValue(json, type);
-        } catch (Exception e) {
-            LOGGER.error("convert JSON to POJO failure", e);
-            throw new RuntimeException(e);
-        }
+        pojo = JSON.parseObject(json, type);
         return pojo;
     }
 }
